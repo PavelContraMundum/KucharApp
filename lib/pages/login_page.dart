7e87f636d9efd2a47fd60d95a4d8718ebb5ae856
PavelContraMundum <pavel.sticka@gmail.com>
2024-08-04 14:46:00 +0200

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,59 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Login"),
+      ),
+      body: SafeArea(child: _buildUI()),
+    );
   }
+
+
+  Widget _buildUI() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _title(),
+          _loginForm(),
+        ],),
+    );
+  }
+
+  Widget _title(){
+    return const Text("Recipe Book", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300),);
+  }
+
+  Widget _loginForm(){
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.90,
+      height: MediaQuery.sizeOf(context).height * 0.30,
+      child: Form(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          TextFormField(decoration: InputDecoration(hintText: "Username"),),
+
+          TextFormField(decoration: InputDecoration(hintText: "Password"),),
+          _loginButton(),
+        ],
+      )),
+    );
+  }
+
+ Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.60,
+      child: ElevatedButton(onPressed: (){}, child: const Text("Login"),),
+    );
+ }
 }
+
